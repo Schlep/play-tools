dafd80857e8cf3e6e6a790227502217955a45d11
Schlep <hischlep@gmail.com>
2026-05-09 22:23:41 +1000

    Add discogs-photo-resize

--- discogs-photo-resize/flake.nix
+++ discogs-photo-resize/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A very basic flake";
+  description = "discogs-photo-resize: resize photos for discogs";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
